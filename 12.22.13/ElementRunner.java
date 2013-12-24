ca0cf23f0550231f97c53f1718166b7bb0b70635
Thomas-F <tlfahrner@gmail.com>
2013-12-23 22:17:42 -0800

    Update ElementRunner.java

--- 12.22.13/ElementRunner.java
+++ 12.22.13/ElementRunner.java
@@ -6,10 +6,10 @@ public class ElementRunner
 	public static void main(String[] args)
 	{
 		Scanner myScanner = new Scanner(System.in);
-		System.out.println("Please enter two elements that you would like to find the properties of their bond.");
-		System.out.print("Enter element 1:");
+		System.out.println("Please enter two elements of which you would like to find the properties of their bond.");
+		System.out.print("Enter element one:");
 		String symbolOne = myScanner.nextLine();
-		System.out.print("Enter element 2:");
+		System.out.print("Enter element two:");
 		String symbolTwo = myScanner.nextLine();
 		myScanner.close();
 		DatabaseReader myDatabaseReader = new DatabaseReader(symbolOne, symbolTwo);
